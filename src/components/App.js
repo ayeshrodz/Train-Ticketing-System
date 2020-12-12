c35ef10b48ddf6816736b4d0bc361e54098ff680
ayeshrodz <ayeshrodz@gmail.com>
2020-12-12 10:10:01 +0530

    Signup page dev

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import Signup from "./auth/Signup"
+import { Container } from 'react-bootstrap'
+
+function App() {
+  return (
+    <Container 
+      className="d-flex align-items-center justify-content-center" 
+      style={{ minHeight: "100vh" }}>
+        <div className="w-100" style= {{ maxWidth: '400px'}}>
+          <Signup/>
+        </div>
+    </Container>
+  )
+}
+
+export default App;
