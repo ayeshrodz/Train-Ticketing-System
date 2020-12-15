ee13b4145613881a47b72512315fde9c09056b49
ayeshrodz <ayeshrodz@gmail.com>
2020-12-15 22:48:57 +0530

    Complete signup page

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
+import React from "react"
 import Signup from "./auth/Signup"
-import { Container } from 'react-bootstrap'
+import { Container } from "react-bootstrap"
+import { AuthProvider } from "../contexts/AuthContext"
 
 function App() {
   return (
-    <Container 
-      className="d-flex align-items-center justify-content-center" 
-      style={{ minHeight: "100vh" }}>
-        <div className="w-100" style= {{ maxWidth: '400px'}}>
-          <Signup/>
-        </div>
-    </Container>
+    <AuthProvider>
+      <Container 
+        className="d-flex align-items-center justify-content-center" 
+        style={{ minHeight: "100vh" }}>
+          <div className="w-100" style= {{ maxWidth: "400px"}}>
+            <Signup/>
+          </div>
+      </Container>
+    </AuthProvider>
+    
   )
 }
 
