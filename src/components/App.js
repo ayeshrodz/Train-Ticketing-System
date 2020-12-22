4b22c3760044f177502354f52b0f79a7df3df9ee
ayeshrodz <ayeshrodz@gmail.com>
2020-12-23 01:48:39 +0530

    temp-change-for-sync

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,16 @@ import React from "react"
 import Signup from "./auth/Signup"
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../contexts/AuthContext"
+import AuthMain from "./auth/AuthMain"
 
 function App() {
   return (
     <AuthProvider>
-      <Container 
-        className="d-flex align-items-center justify-content-center" 
-        style={{ minHeight: "100vh" }}>
-          <div className="w-100" style= {{ maxWidth: "400px"}}>
-            <Signup/>
-          </div>
-      </Container>
+      {/* Header */}
+
+      {/* Login */}
+      <AuthMain/>
+
     </AuthProvider>
     
   )
