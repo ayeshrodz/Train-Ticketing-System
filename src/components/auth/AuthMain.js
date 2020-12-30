9e8752f7c58c10773f5a252a8591de17eaabd2c4
ayeshrodz <ayeshrodz@gmail.com>
2020-12-31 02:07:48 +0530

    Auth main page migrate styles to css file

--- src/components/auth/AuthMain.js
+++ src/components/auth/AuthMain.js
@@ -2,16 +2,14 @@ import React from "react";
 import Signup from "./Signup";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../../contexts/AuthContext";
+import "./AuthMain.css";
 
 export default function AuthMain() {
   return (
     <div className="auth_main">
       <AuthProvider>
-        <Container
-          className="d-flex align-items-center justify-content-center"
-          style={{ minHeight: "100vh" }}
-        >
-          <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Container className="d-flex align-items-center justify-content-center sign-up-main">
+          <div className="w-100 sign-up-content">
             <Signup />
           </div>
         </Container>
