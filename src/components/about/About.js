d316a8cec931732637658c0257b94c674dc5f85e
Rashitha97 <76090980+Rashitha97@users.noreply.github.com>
2021-01-18 21:33:25 +0530

    User profile js edited

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -15,12 +15,13 @@ import image11 from '../Images/train11.jpg';
 function About() {
   return (
     <div className="about">
-      <Carousel>
+      <Carousel >
   <Carousel.Item>
     <img
       className="d-block w-100"
       src={image1} 
       alt="First slide"
+      
     />
   </Carousel.Item>
 
