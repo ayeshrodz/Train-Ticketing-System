46e658e09233a2b5bdc90a8345327157548431f0
maharshif <70820109+maharshif@users.noreply.github.com>
2021-01-19 15:34:49 +0530

    aboutus final

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,20 +1,32 @@
 import React from "react";
-import image1 from '../Images/train1.jpg';
 import "./About.css";
 
 function About() {
   return (
-    <div className="about">
+    <div className="about" align="center">
+      <div className="heading">
+        <h1>About Us</h1>
+      </div>
+      <div className="para1">
+        <p>
+          If you’re exploring Sri Lanka for the first time or embarking on your
+          next Sri Lankan adventure, traveling by train is the ideal option to
+          explore the beautiful cities and landscapes of Sri Lanka. We are here
+          to make sure your Sri Lankan journey will remain remarkable. CHIN-CHIN
+          makes it easy and affordable to buy Sri Lankan train tickets online.
+          For daily travelers to far away journey travelers, we let you book
+          your train journey easily, anytime, anywhere. CHIN-CHIN was launched
+          in 2021 where our primary focus is to help our customers connect their
+          interests by train, island wide. We are dedicated to offer the best
+          experience to our customers. We look forward to meeting your Sri
+          Lankan train tickets needs.
+        </p>
 
-          <img
-            className="background"
-            src={image1}
-            
-          />
-        
-     
+        <div className="subhead">
+          <h3>Plan your Journey with us!</h3>
+        </div>
+      </div>
     </div>
-
   );
 }
 
