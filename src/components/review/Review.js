d1e1b5e17355249607aa9139e8dabb11628229ff
Anupa <anupa@aventude.com>
2021-01-20 19:22:30 +0530

    Small CHanges

--- src/components/review/Review.js
+++ src/components/review/Review.js
@@ -23,7 +23,7 @@ function Review() {
       <div className = "review_header">
         <Avatar/>
         <div className = "review_info">
-          <h2>Anupa Indula</h2>
+          <h2>Himasara</h2>
           <p>Review</p>
         </div>
       </div>
