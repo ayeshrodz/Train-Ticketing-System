731cfda0a16ee8e51b632ad8bd7ada66e10d0d2e
Anupa Indula <anupaindula50@gmail.com>
2021-01-23 22:46:12 +0530

    Changes in Home Page

---
+++ src/components/home/From.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import firestore from "../../firebase";
+import Form from "react-bootstrap/Form";
+
+export default function From() {
+    const [Trainschedules, setSchedules] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const ref = firestore.firestore().collection("TrainSchdule");
+  const defaultProps = {
+    options: Trainschedules,
+    getOptionLabel: (TrainSchdule) => TrainSchdule.title,
+  };
+  function getSchedules() {
+    setLoading(true);
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+      });
+      setSchedules(items);
+      setLoading(false);
+    });
+  }
+  const flatProps = {
+    TrainSchdule: Trainschedules.map((TrainSchdule) => TrainSchdule.StartStation),
+  };
+
+  const [value, setValue] = React.useState(null);
+
+  return (
+    <div style={{ width: 300 }}>
+     <Form className="Search-Form">
+      <div>
+      
+      <Autocomplete
+      className="input-search"
+      {...defaultProps}
+      id="clear-on-escape"
+      clearOnEscape
+      renderInput={(params) => <TextField {...params} label="From" margin="normal" />}
+    />
+    
+      </div>
+
+      <div>
+          
+      
+    
+      </div>
+      </Form>  
+    </div>
+  );
+}
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,43 +1,90 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import "./Home.css";
 import TextField from "@material-ui/core/TextField";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
-import IconButton from '@material-ui/core/IconButton';
-
+import IconButton from "@material-ui/core/IconButton";
+import From from "./From";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import firestore from "../../firebase";
 
 function Home() {
+  const [fromKeyword, setInput] = useState("");
+  const [toKeyword, setSearchValue] = useState("");
 
-  const [fromKeyword, setInput] = useState ("");
-  const [toKeyword, setSearchValue] = useState ("");
+  const [Trainschedules, setSchedules] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const ref = firestore.firestore().collection("TrainSchdule");
+  const defaultProps = {
+    options: Trainschedules,
+    getOptionLabel: (TrainSchdule) => TrainSchdule.title,
+  };
+  function getSchedules() {
+    setLoading(true);
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+      });
+      setSchedules(items);
+      setLoading(false);
+    });
+  }
+  const flatProps = {
+    TrainSchdule: Trainschedules.map(
+      (TrainSchdule) => TrainSchdule.StartStation
+    ),
+  };
+
+  const [value, setValue] = React.useState(null);
 
   var curr = new Date();
-curr.setDate(curr.getDate() + 0);
-var date = curr.toISOString().substr(0,10);
+  curr.setDate(curr.getDate() + 3);
+  var date = curr.toISOString().substr(0, 10);
 
-  function fetchData () {
-    if(fromKeyword !== "" && toKeyword !== "") {
-      window.location.href = '/searchresult?from='+ fromKeyword +  toKeyword
-    }else {
-      alert("no value")
+  function fetchData() {
+    if (fromKeyword !== "" && toKeyword !== "") {
+      window.location.href = "/searchresult?from=" + fromKeyword + toKeyword;
+    } else {
+      alert("no value");
     }
   }
   return (
-    <div className = "bgcr">
-      <div className="home1">
-     <div align = "center" className = "chin">
-     <h2 className="topic-2">Plan Your Journey With Us</h2>
-     </div>
+    <div className="home1">
+      <h2 className="topic-2">Plan Your Journey With Us</h2>
       <Form className="Search-Form">
-        <div className="input-search">
-          <input placeholder="From" value = {fromKeyword} onChange = {e => setInput(e.target.value)}/>
+        <div>
+          {/* <input placeholder="From" value = {fromKeyword} onChange = {e => setInput(e.target.value)}/> */}
+          <div className="input-search">
+            <Autocomplete
+              className="input-search"
+              {...defaultProps}
+              id="clear-on-escape"
+              clearOnEscape
+              value={fromKeyword}
+                onChange={(e) => setInput(e.target.value)}
+              renderInput={(params) => (
+                <TextField {...params} label="From" margin="normal"   />
+              )}
+            />
+          </div>
         </div>
         <div className="input-search">
-          <input placeholder="To" value = {toKeyword} onChange = {e => setSearchValue(e.target.value)}/>
+          <Autocomplete
+            className="input-search"
+            {...defaultProps}
+            id="clear-on-escape"
+            clearOnEscape
+            value={toKeyword}
+              onChange={(e) => setInput(e.target.value)}
+            renderInput={(params) => (
+              <TextField {...params} label="To" margin="normal"  />
+            )}
+          />
         </div>
         <div className="DatePicker-Search">
           <TextField
-            type = "date"
+            type="date"
             id="date"
             label="Date"
             type="date"
@@ -48,11 +95,10 @@ var date = curr.toISOString().substr(0,10);
             }}
           />
         </div>
-        <div className = "next-btn-home">
-              <ArrowForwardIcon className="arrow-right-home" onClick = {fetchData}/> 
+        <div className="next-btn-home">
+          <ArrowForwardIcon className="arrow-right-home" onClick={fetchData} />
         </div>
       </Form>
-    </div>
     </div>
   );
 }
