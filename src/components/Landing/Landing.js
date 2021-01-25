3efa51521f947d1592b81eddf959d4aaa9af16c0
ayeshrodz <ayeshrodz@gmail.com>
2021-01-26 01:30:22 +0530

    config firebase hosting and landing page changes

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,28 +1,34 @@
 import React from "react";
 import "./Landing.css";
-import logo from "../Images/bg-1.jpeg";
 import { Link } from "react-router-dom";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import Image from "react-bootstrap/Image";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 function Landing() {
   return (
-    <div className="bgcr">
-      <div className="landing">
-        <div align="center" className="CHIN-CHIN">
-          <img src={logo} className="logo" alt="logo" />
-          <h1 className="slogan">Chin-Chin</h1>
-          <h2 className = "heading-2">Online Train Ticket Booking Platform</h2>
-        </div>
-
-        <div className="arrow" width="100%">
-          <div className="next-btn">
-            <Link to="/home">
-              <ArrowForwardIcon className="arrow-right" />
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col className="lg-6">
+          <Image className="logo" src="/img/landing.jpeg" fluid />
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center text-center ">
+        <Col md="auto">
+          <h1 className="heading-1">Chin-Chin</h1>
+          <h2 className="heading-2">Online Train Ticket Booking Platform</h2>
+        </Col>
+      </Row>
+      <Row className="justify-content-center text-center ">
+        <Col className="p-4">
+          <Button variant="outline-warning">
+            <Link className="go-to-link" to="/home">
+              Get Start <ArrowForwardIcon />
             </Link>
-          </div>
-        </div>
-      </div>
-    </div>
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
