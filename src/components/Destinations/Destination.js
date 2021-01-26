259cb8099c55affb3e33f04a2365c0ca5c25337c
Anupa Indula <anupaindula50@gmail.com>
2021-01-27 04:54:53 +0530

    Destination Page Completed

--- src/components/Destinations/Destination.js
+++ src/components/Destinations/Destination.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./Destination.css";
-import SearchIcon from "@material-ui/icons/Search";
-import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import firestore from "../../firebase";
 
 function Destination() {
+  const ref = firestore.firestore().collection("destinations");
+  const [destination, setDestination] = useState([]);
+
+
+  function getDestination() {
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+      });
+      setDestination(items);
+    });
+  }
+
+  useEffect(() => {
+    getDestination();
+  }, []);
+
+
+
   return (
     <div className="row desination-row">
-      <div className="card-wrapper"></div>
-      <div className="card-wrapper"></div>
-      <div className="card-wrapper"></div>
+      {destination.map((destination) => (
+      <div className="card-wrapper"><img className="card-wrapper" src = {destination.destiimage}/>
+       <div className = "destination-topic"> <h3 >{destination.destiDescription}</h3></div>
+      </div>
+  
+      ))};
     </div>
   );
 }
