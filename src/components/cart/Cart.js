c151bff93945052a87d29770528f2c2d5354df5b
Anupa Indula <anupaindula50@gmail.com>
2021-01-31 01:56:21 +0530

    Cart Page Done

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,12 +1,78 @@
-import React from "react";
-import "./Cart.css";
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from "../../contexts/AuthContext";
+import { Link, useHistory } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
-function Cart(props) {
+
+export default function Cart() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signup, currentUser } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match");
+    }
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+      if (!currentUser) {
+        console.log(currentUser.uid);
+      }
+      history.push("/profile");
+    } catch {
+      setError("Failed to create an account");
+    }
+    setLoading(false);
+  }
+
   return (
-    <div>
-      <h1>Cart Items</h1>
-    </div>
+    <>
+    <Container className="d-flex align-items-center justify-content-center sign-up-main">
+          <div className="w-100 sign-up-content">
+          <Card className="shadow p-3 mb-5 ml-0 mt-0 bg-white rounded card">
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign Up</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Card Number</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>CVV</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Exp Date</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Card Name</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
+            </Form.Group>
+            <Button
+              disabled={loading}
+              className="w-100"
+              style={{ background: "#f25e42", border: "none" }}
+              type="submit"
+            >
+              Pay Now
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+          </div>
+      </Container>
+      
+    </>
   );
 }
-
-export default Cart;
